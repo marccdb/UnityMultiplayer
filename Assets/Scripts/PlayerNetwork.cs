fde57c8f62b7ababdb54a86319302f07cdcbf338
Marcio Del Bianco <marcio.bianco@gmail.com>
2022-10-20 15:42:06 -0300

    add basic multiplayer host/client

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
     float moveVelocity = 6;
 
     void Update()
     {
+
+        if (!IsOwner) return;
 
         float _moveVertical = Input.GetAxis("Vertical") * moveVelocity * Time.deltaTime;
         //float _moveHorizontal = Input.GetAxis("Horizontal") * _velocity * Time.deltaTime;
